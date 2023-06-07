3058d18567023bb3c6c896ae13409ad018771841
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-06-07 19:42:39 +0200

    Test test

--- app/src/main/java/com/meditationtimer/MainActivity.kt
+++ app/src/main/java/com/meditationtimer/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MeditationTimerTheme {
                 // A surface container using the 'background' color from the theme
+                // test
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
